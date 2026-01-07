b652c38deb3b347438671188a3d3d1a5512bc39b
Maria Luiza Lima <luizagamesl90@gmail.com>
2026-01-07 15:44:15 -0300

    Components, barra superior e estrutura

---
+++ widgets/BarraSuperior.tsx
@@ -0,0 +1,19 @@
+import Logo from "@/components/Logo/Logo";
+import Link from "next/link";
+
+const BarraSuperior = () => {
+    return (
+        <>
+            <div className="container py-6 mx-auto flex justify-between items-center">
+            <Logo/>
+            <div className="flex gap-6">
+                <Link className="font-semibold" href="/">Acomodações</Link>
+                <Link className="opacity-60" href="/">Experiências</Link>
+            </div>
+            <button className="rounded-md border-2">Entrar</button>
+            </div>        
+        </>
+    );
+}
+
+export default BarraSuperior;
